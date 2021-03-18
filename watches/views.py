edfb0450e86f7910002aa5a26cf495ca83133c38
l0nelyhermit <mrjonathangoh@gmail.com>
2021-03-18 15:45:32 +0000

    edit form route completed and save to database

--- watches/views.py
+++ watches/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404, reverse
 from .models import Watch
 from .forms import WatchForm
 
@@ -32,6 +32,19 @@ def show_post(request):
 
 def edit_post(request, item_id):
     watch_edited = get_object_or_404(Watch, pk=item_id)
+    if request.method == "POST":
+        # load the updated form from the html
+        modified_form = WatchForm(request.POST, instance=watch_edited)
+        if modified_form.is_valid():
+            # process the form and save to database if it is valid
+            form = modified_form.save(commit=False)
+            form.save()
+            return redirect(reverse(show_post))
+        else:
+            # return to form if invalid form
+            return render(request, 'edit_post.html', {
+                'form': modified_form
+            })
     form = WatchForm(instance=watch_edited)
     return render(request, 'edit_post.html', {
         'form': form
