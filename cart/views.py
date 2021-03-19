0625f8206de18ac18c43debf2a44ae680eb4f2db
l0nelyhermit <mrjonathangoh@gmail.com>
2021-03-19 12:23:56 +0000

    render cart to show watch selected for payment

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from watches.models import Watch
 # Create your views here.
+
+
 def cart(request, item_id):
-    return render(request, 'cart.html')
+    watch = get_object_or_404(Watch, pk=item_id)
+    return render(request, 'cart.html', {
+        'watch': watch
+    })
