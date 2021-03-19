3e01f384510f669ed93816d41a0eaec8053cd6f5
l0nelyhermit <mrjonathangoh@gmail.com>
2021-03-19 13:57:13 +0000

    update allowed host

--- Timeless/settings.py
+++ Timeless/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://john-p4-timeless.herokuapp.com/','*']
 
 
 # Application definition
